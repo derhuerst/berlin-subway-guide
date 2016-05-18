df0ebbc0a45bef65bc93d290d3f2f27530ccc150
Jannis R <mail@jannisr.de>
2016-05-18 03:10:42 +0200

    hotlink photos
    unfortunately, deploying is hard with github and binary data like photos. and because i'm lazy and flickr probably doesn't care about this small amount of hotlinked traffic, why not?

--- build.js
+++ build.js
@@ -4,15 +4,12 @@ const fs      = require('fs')
 const path    = require('path')
 const pick    = require('lodash.pick')
 const photos  = require('vbb-station-photos')
+const flickr  = require('flickr-photo-url')
 const queue   = require('queue')
 const lookup  = require('vbb-stations')
 
 const pkg    = require('./package.json')
 const tpl    = require('./src/template')
-
-
-
-fs.symlinkSync(photos.dir, path.join(__dirname, 'photos'))
 
 
 
@@ -34,22 +31,26 @@ Object.keys(photos.list).forEach((id) => {
 	Object.keys(station).forEach((line) => {
 		const set = station[line]
 		if (!set.label) return
-		q.push((next) => lookup(true, +id).catch(next).then((r) => {
+		q.push((next) =>
+			Promise.all([
+				lookup(true, +id),
+				flickr('ingolfbln', set.label, 'z')
+			]).catch(next)
+			.then((all) => {
 
-			const file = path.relative(photos.dir, photos(id, line, 'label'))
-			platforms.push({
-				  station: r[0].name
-				, line:    line
-				, img:     path.join('photos', file)
-			})
+				platforms.push({
+					  station: all[0][0].name
+					, line:    line
+					, img:     all[1]
+				})
 
-			next()
-		}).catch(next))
+				next()
+			}).catch(next))
 	})
 })
 
 q.start((err) => {
-	if (err) return console.error(err.message)
+	if (err) return console.error(err.stack)
 
 	const html = tpl(_, platforms.sort(sortPlatforms))
 	const dest = path.join(__dirname, 'index.html')
