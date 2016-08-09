48e9cf06f14770430750aa4af8ee3b6695105724
Jannis R <mail@jannisr.de>
2016-08-09 05:53:46 +0200

    rewrite build script

--- build.js
+++ build.js
@@ -1,57 +1,38 @@
 'use strict'
 
-const fs      = require('fs')
 const path    = require('path')
-const pick    = require('lodash.pick')
 const photos  = require('vbb-station-photos')
-const flickr  = require('flickr-photo-url')
-const queue   = require('queue')
 const lookup  = require('vbb-stations')
+const fs      = require('fs')
+const pick    = require('lodash.pick')
 
 const pkg    = require('./package.json')
 const tpl    = require('./src/template')
 
 
 
-const _ = pick(pkg, ['title', 'description', 'author'])
-
 let platforms = {}
-const q = queue()
-q.concurrency = 5
 
-Object.keys(photos.list).forEach((id) => {
-	const station = photos.list[id]
-	Object.keys(station).forEach((line) => {
-		const set = station[line]
-		if (!set.label) return
+const byStation = photos.list
+for (let id in byStation) {
+	const station = lookup(+id)[0]
+	if (!station) continue
+	const byLine = byStation[id]
+	for (let line in byLine) {
+		const set = byLine[line]
+		if (!set.label) continue
+
+		const src = photos(id, line, 'label')
+		if (!src) continue
+		const base = path.basename(src)
+		const dest = path.join(__dirname, 'photos', base)
+		fs.linkSync(src, dest)
+
 		if (!(line in platforms)) platforms[line] = []
-		q.push((next) =>
-			Promise.all([
-				lookup(true, +id),
-				flickr(set.label[0], set.label[1], 'z')
-			]).then((all) => {
+		platforms[line].push({station: station.name, line, img: base})
+	}
+}
 
-				platforms[line].push({
-					  station: all[0][0].name
-					, line:    line
-					, img:     all[1]
-				})
-
-				console.info(line, all[0][0].name, '✓')
-				next()
-			}, next).catch(next))
-	})
-})
-
-// q.on('error', (err) => {
-// 	console.error(err.message)
-// 	q.start()
-// })
-q.start(() => {
-	const html = tpl(_, platforms)
-	const dest = path.join(__dirname, 'index.html')
-	fs.writeFile(dest, html, (err) => {
-		if (err) return console.error(err.stack)
-		console.log('done')
-	})
-})
+const html = tpl(pick(pkg, ['title', 'description', 'author']), platforms)
+const dest = path.join(__dirname, 'index.html')
+fs.writeFileSync(dest, html)
